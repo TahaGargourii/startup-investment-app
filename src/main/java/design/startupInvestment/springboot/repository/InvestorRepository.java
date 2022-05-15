4629918aa28573aae424fd842936cdd172155bac
TahaaG <gargouri.tahaa@gmail.com>
2022-05-15 19:46:32 +0200

    clean deploy folder

--- src/main/java/design/startupInvestment/springboot/repository/InvestorRepository.java
+++
@@ -1,8 +0,0 @@
-package design.startupInvestment.springboot.repository;
-
-import design.startupInvestment.springboot.model.Investor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface InvestorRepository extends JpaRepository<Investor, Long> {
-}
--- src/main/java/design/startupInvestment/springboot/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package design.startupInvestment.springboot.repository;
-
-import design.startupInvestment.springboot.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
